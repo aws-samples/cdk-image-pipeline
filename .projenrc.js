bc3254a967eff4ab289c5733d84743d68189cfdf
Cameron Magee <magcamer@amazon.com>
2023-06-27 10:45:53 -0400

    chore(relicense): switch to Apache-2.0 license

--- .projenrc.js
+++ .projenrc.js
@@ -14,7 +14,7 @@ const project = new awscdk.AwsCdkConstructLibrary({
     distName: 'cdk-image-pipeline',
     module: 'cdk_image_pipeline',
   },
-  license: 'MIT-0',
+  license: 'Apache-2.0',
   pullRequestTemplateContents: ['# Fixes', ' ', ' ', 'By submitting this pull request, I confirm that you can use, modify, copy, and redistribute this contribution, under the terms of your choice.'],
   releaseToNpm: true,
   devDeps: ['@types/prettier@2.7.2'],
